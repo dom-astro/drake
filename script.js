069d67c584e4395e5c9b08197c81c7e15afeb23f
Dom Astro <94188743+dom-astro@users.noreply.github.com>
2024-12-06 11:58:12 +0100

    Ajout des scénaris pessimistes et optimistes

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 document.getElementById('calcul').addEventListener('click', function(event) {
     event.preventDefault();
 
-    const R = parseFloat(document.getElementById('R').value);
-    const fp = parseFloat(document.getElementById('fp').value);
-    const ne = parseFloat(document.getElementById('ne').value);
-    const fl = parseFloat(document.getElementById('fl').value);
-    const fi = parseFloat(document.getElementById('fi').value);
-    const fc = parseFloat(document.getElementById('fc').value);
-    const L = parseFloat(document.getElementById('L').value);
+    const R_pessimiste = parseFloat(document.getElementById('R-pessimiste').value);
+    const fp_pessimiste = parseFloat(document.getElementById('fp-pessimiste').value);
+    const ne_pessimiste = parseFloat(document.getElementById('ne-pessimiste').value);
+    const fl_pessimiste = parseFloat(document.getElementById('fl-pessimiste').value);
+    const fi_pessimiste = parseFloat(document.getElementById('fi-pessimiste').value);
+    const fc_pessimiste = parseFloat(document.getElementById('fc-pessimiste').value);
+    const L_pessimiste = parseFloat(document.getElementById('L-pessimiste').value);
 
-    const N = R * fp * ne * fl * fi * fc * L;
+    const R_optimiste = parseFloat(document.getElementById('R-optimiste').value);
+    const fp_optimiste = parseFloat(document.getElementById('fp-optimiste').value);
+    const ne_optimiste = parseFloat(document.getElementById('ne-optimiste').value);
+    const fl_optimiste = parseFloat(document.getElementById('fl-optimiste').value);
+    const fi_optimiste = parseFloat(document.getElementById('fi-optimiste').value);
+    const fc_optimiste = parseFloat(document.getElementById('fc-optimiste').value);
+    const L_optimiste = parseFloat(document.getElementById('L-optimiste').value);
 
-    document.getElementById('result').innerText = `Nombre estimé de civilisations: ${N.toFixed(2)}`;
+    const N_pessimiste = R_pessimiste * fp_pessimiste * ne_pessimiste * fl_pessimiste * fi_pessimiste * fc_pessimiste * L_pessimiste;
+    const N_optimiste = R_optimiste * fp_optimiste * ne_optimiste * fl_optimiste * fi_optimiste * fc_optimiste * L_optimiste;
+
+    document.getElementById('result').innerText = `Nombre estimé de civilisations: ${N_pessimiste.toFixed(2)} / ${N_optimiste.toFixed(2)}`;
 });
 
 $("#R").on('click', function(event) {
@@ -49,5 +58,4 @@ function show_variable(varDrake) {
     }
     $(".explication").hide();
     $("#explication-"+varDrake).show(showTime);
-
 }
